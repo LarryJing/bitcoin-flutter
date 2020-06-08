526af820b374efd0901d46233e6f30fc9ecb6357
Lawrence <lajing@umich.edu>
2020-06-08 15:30:22 -0700

    gold medal challenge complete, all 3 cryptocurrencies are working

--- lib/price_screen.dart
+++ lib/price_screen.dart
@@ -10,7 +10,9 @@ class PriceScreen extends StatefulWidget {
 
 class _PriceScreenState extends State<PriceScreen> {
   static String selectedCurrency = 'USD';
-  String rate = 'Loading...';
+  String rateBTC = 'Loading...';
+  String rateETH = 'Loading...';
+  String rateLTC = 'Loading...';
   DropdownButton<String> androidDropdown() {
     List<DropdownMenuItem<String>> menuItems = [];
     for (String string in currenciesList) {
@@ -26,7 +28,9 @@ class _PriceScreenState extends State<PriceScreen> {
       onChanged: (value) {
         setState(() {
           selectedCurrency = value;
-          getRate(CoinData('BTC', selectedCurrency));
+          getRate('BTC');
+          getRate('ETH');
+          getRate('LTC');
         });
       },
     );
@@ -43,25 +47,87 @@ class _PriceScreenState extends State<PriceScreen> {
       itemExtent: 30.0,
       onSelectedItemChanged: (selected) {
         selectedCurrency = list[selected].data;
-        getRate(CoinData('BTC', selectedCurrency));
+        getRate('BTC');
+        getRate('ETH');
+        getRate('LTC');
       },
       children: list,
     );
   }
 
+  List<Widget> cardList() {
+    List<Widget> l = [];
+    String rate;
+    for (String s in cryptoList) {
+      if (s == 'BTC') {
+        rate = rateBTC;
+      } else if (s == 'ETH') {
+        rate = rateETH;
+      } else if (s == 'LTC') {
+        rate = rateLTC;
+      } else {
+        rate = "Unavailable";
+      }
+      l.add(
+        Padding(
+          padding: EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 0),
+          child: Card(
+            color: Colors.lightBlueAccent,
+            elevation: 5.0,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10.0),
+            ),
+            child: Padding(
+              padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
+              child: Text(
+                '1 $s = $rate $selectedCurrency',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 20.0,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+        ),
+      );
+    }
+    l.add(
+      Container(
+        height: 150.0,
+        alignment: Alignment.center,
+        padding: EdgeInsets.only(bottom: 30.0),
+        color: Colors.lightBlue,
+        child: Platform.isIOS ? iOSPicker() : androidDropdown(),
+      ),
+    );
+    return l;
+  }
+
   @override
   void initState() {
     super.initState();
-    getRate(CoinData('BTC', selectedCurrency));
+    getRate('BTC');
+    getRate('ETH');
+    getRate('LTC');
   }
 
-  void getRate(CoinData coin) async {
+  void getRate(String crypt) async {
+    CoinData coin = CoinData(crypt, selectedCurrency);
     var data = await coin.getCoinData();
     setState(() {
       if (data != null) {
-        rate = data['rate'].toInt().toString();
+        if (crypt == 'BTC') {
+          rateBTC = data['rate'].toInt().toString();
+        } else if (crypt == 'ETH') {
+          rateETH = data['rate'].toInt().toString();
+        } else {
+          rateLTC = data['rate'].toInt().toString();
+        }
       } else {
-        rate = '0';
+        rateBTC = 'Unavailable';
+        rateETH = 'Unavailable';
+        rateLTC = 'Unavailable';
       }
     });
   }
@@ -75,36 +141,7 @@ class _PriceScreenState extends State<PriceScreen> {
       body: Column(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: <Widget>[
-          Padding(
-            padding: EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 0),
-            child: Card(
-              color: Colors.lightBlueAccent,
-              elevation: 5.0,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10.0),
-              ),
-              child: Padding(
-                padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
-                child: Text(
-                  '1 BTC = $rate $selectedCurrency',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 20.0,
-                    color: Colors.white,
-                  ),
-                ),
-              ),
-            ),
-          ),
-          Container(
-            height: 150.0,
-            alignment: Alignment.center,
-            padding: EdgeInsets.only(bottom: 30.0),
-            color: Colors.lightBlue,
-            child: Platform.isIOS ? iOSPicker() : androidDropdown(),
-          ),
-        ],
+        children: cardList(),
       ),
     );
   }
